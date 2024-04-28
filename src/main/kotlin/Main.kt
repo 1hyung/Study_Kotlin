3c59a1165518c91df3d39d5511d1521af70cf5b7
1hyung <ryu1hyung@naver.com>
2024-04-28 11:19:00 +0900

    2-11 과제 초안

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-/*
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+/*//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val name = "Kotlin"
@@ -14,8 +13,10 @@ fun main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
-}
-*/
-fun main() {
-    println("Hello Kotlin")
+}*/
+// 2-11 과제
+fun main () {
+    println("메뉴를 선택해주세요.")
+    println("1. 게임이 시작되었습니다.")
+    println("2. 게임이 종료되었습니다.")
 }
