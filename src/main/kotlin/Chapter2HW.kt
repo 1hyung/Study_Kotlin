6227067dc532e26a54ead9b827d4c4577311c374
1hyung <ryu1hyung@naver.com>
2024-04-30 16:49:56 +0900

    lv1. when식 함수 적용 후

--- src/main/kotlin/Chapter2HW.kt
+++ src/main/kotlin/Chapter2HW.kt
@@ -1,5 +1,5 @@
 package org.example
-
+//lv1.
 fun main() {
     println("메뉴를 입력해주세요.")
     println("1. 게임 시작하기")
@@ -7,8 +7,16 @@ fun main() {
 // 입력 받을 것을 변수에 넣을거다.
     val selcectedMenu = readLine()!!.toInt() // 사람이 넣은 값을 Int로 바꿈
 
-    when (selcectedMenu) {
-        1 -> println("게임이 시작되었습니다.")
-        2 -> println("게임이 종료되었습니다.")
+    var value = when (selcectedMenu) {
+        1 -> startGame()
+        2 -> endGame()
     }
+}
+
+fun startGame() {
+    println("게임이 시작되었습니다.")
+}
+
+fun endGame() {
+    println("게임이 종료되었습니다.")
 }
