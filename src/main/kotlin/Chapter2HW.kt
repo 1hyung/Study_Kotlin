330748259239d07c49f834f3ceae19017d86d32c
1hyung <ryu1hyung@naver.com>
2024-04-30 17:34:35 +0900

    lv2. null처리 2번 name과 job 변수에 elvis연산자 적용 ?: ""

--- src/main/kotlin/Chapter2HW.kt
+++ src/main/kotlin/Chapter2HW.kt
@@ -22,14 +22,15 @@ fun startGame() {
 }
 
 fun createCharacter() {
-    val characterList: MutableList<Map<String, String?>> = mutableMapOf()// 리스트 타입 지정해야함. 리스트에서는 똑같은 타입 하나만 넣을 수 있음.
+    val characterList: MutableList<Map<String, String>> = mutableMapOf()// 리스트 타입 지정해야함. 리스트에서는 똑같은 타입 하나만 넣을 수 있음.
+   // 1번 처리 val characterList: MutableList<Map<String, String?>> = mutableMapOf()
     //키가 string, value도 string
 
-    println("추가할 캐릭터의 이름을 엽력해주세요.")
-    val name = readLine()
+    println("추가할 캐릭터의 이름을 엽력해주세요.") //2번 Evis연산자 사용
+    val name = readLine() ?: ""
 
     println("추가할 캐릭터의 직업을 입력해주세요.")
-    val job = readLine()
+    val job = readLine() ?: ""
     // 이름과 직업이 항상 같이 다니면 편하다. map 자료구조로 만들어서 다룰 것이다. 키와 값
     val character = mapOf("name" to name, "job" to job) // 캐릭터 1개 만드는 것
     //이제 리스트에 캐릭터를 넣는 코르를 만들거다.
@@ -40,7 +41,7 @@ fun createCharacter() {
     //characterList는 string?가 아니라 그냥 string 타입이다.
     //해결 방법은 2가지가 있음.
     //1.characterList에 ?를 붙힌다.
-    //2.name의 readLine의 값이 null일 수 있으니까 null일 때 어떻게 처리할지 지정을 할 수 있다.
+    //2.name의 readLine의 값이 null일 수 있으니까 null일 때 어떻게 처리할지 지정을 할 수 있다. Elvis연산자 사용 ?: ""
 }
 
 fun endGame() {
