a0a750bf9b5a753fde800afd938c0601adac2a0d
1hyung <ryu1hyung@naver.com>
2024-05-01 23:36:22 +0900

    폴더 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "SpartaKotlin"
-
+include("src:main:Project")
+findProject(":src:main:Project")?.name = "Project"
