44c8b1115de885d7cdbbbe8e7623aa21f3de8bd6
1hyung <ryu1hyung@naver.com>
2024-05-05 03:14:44 +0900

    study: 주 생성자, 부 생성자

--- src/main/Project/src/main/kotlin/Main.kt
+++ src/main/Project/src/main/kotlin/Main.kt
@@ -252,3 +252,31 @@ class Character( //클래스를 만들면서 () 소괄호를 열고 닫고 / 소
         return device3
     }
 }
+
+//부 생성자의 사용 예시
+class Character {
+    var name: String = ""
+    var hairColor: String = ""
+    var height: Double = 0.0
+
+    // 명시적 생성자 (Constructor)
+    // _name, _hairColor, _height와 같이 생성자에 변수를 넘기는 경우에 사용함
+    constructor(_name: String, _hairColor: String, _height: Double) {
+        println("${_name}을 생성자로 넘겼어요")
+        println("${_hairColor}를 생성자로 넘겼어요")
+        println("${_height}를 생성자로 넘겼어요")
+
+        name = _name
+        hairColor = _hairColor
+        height = _height
+    }
+
+    fun fireBall() {
+        println("파이어볼!")
+    }
+    fun compositing(device1:String, device2:String): String {
+        val device3 = device1 + device2
+        println("새로운 무기인 ${device3}입니다")
+        return device3
+    }
+}
